b1243bbed6e3523834dac475edfb36069a231d97
adamhfgjfdhtgfhjgkjhghnj <zer0onfortnite16@gmail.com>
2026-04-09 11:25:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function checkQuiz() {
   let score = 0;
 
-  if(document.querySelector('input[name="q1"]:checked')?.value == "1") score++;
-  if(document.querySelector('input[name="q2"]:checked')?.value == "1") score++;
-  if(document.querySelector('input[name="q3"]:checked')?.value == "1") score++;
-  if(document.querySelector('input[name="q4"]:checked')?.value == "1") score++;
+  if(document.querySelector('input[name="q1"]:checked')?.value == "a") score++;
+  if(document.querySelector('input[name="q2"]:checked')?.value == "a") score++;
+  if(document.querySelector('input[name="q3"]:checked')?.value == "a") score++;
+  if(document.querySelector('input[name="q4"]:checked')?.value == "a") score++;
 
   document.getElementById("result").innerText = "Score: " + score + "/4";
 }
